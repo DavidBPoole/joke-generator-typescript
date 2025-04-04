f9bea9541c9646af274866101abb7e343ae71d6e
David Poole <dbpoole7@gmail.com>
2025-04-04 08:46:48 -0500

    building out main index page imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,3 +2,5 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
