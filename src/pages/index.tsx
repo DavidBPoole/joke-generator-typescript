80dbe4b9667b37513d89a041eb61ed238a8408e1
David Poole <dbpoole7@gmail.com>
2025-04-06 14:04:45 -0500

    building out main index page imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,3 +4,5 @@ import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
+import getJoke, { Joke } from "@/api/jokeData";
+import Jokes from "@/components/Jokes";
